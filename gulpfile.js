d82febac49c5ef7021c880006f50328698c4c28a
fareufai <fareufai@gmail.com>
2022-04-22 02:49:11 +0900

    scssの新規追加

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const pug = require("gulp-pug");
+const sass = require("gulp-sass")(require("sass"));
 
 gulp.task("pug", function () {
   return gulp
@@ -10,4 +11,11 @@ gulp.task("pug", function () {
       })
     )
     .pipe(gulp.dest("./public/"));
+});
+
+gulp.task("sass", function () {
+  return gulp
+    .src(["./src/scss/**/*.scss", "!./src/scss/**/_*.scss"])
+    .pipe(sass({ outputStyle: "expanded" }))
+    .pipe(gulp.dest("./public/css"));
 });
